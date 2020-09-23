9f8ee54d7ed11327916f9db178b0557ab54c7ba5
mjam2 <70014295+mjam2@users.noreply.github.com>
2020-09-23 12:27:45 +0200

    Delete uppgift2_6a.java

--- uppgift2_6a.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class uppgift2_6a {
-    public static void main(String[] args) {
-        System.out.println("Hur mycket pengar vill du ta ut?");
-        Scanner uttag = new Scanner(System.in);
-
-    }
-}
